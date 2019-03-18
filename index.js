b3316a592b6580a3e414fae11854b114271be978
Karthikeyan A <karthikaruna@live.com>
2019-03-18 19:10:00 +0530

    Add included for “ember-cli-sass”

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-telephone-keypad'
+  name: 'ember-telephone-keypad',
+  included: function (/* app */) {
+    this._super.included.apply(this, arguments);
+  }
 };
