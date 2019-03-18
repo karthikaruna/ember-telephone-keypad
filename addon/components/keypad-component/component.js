04d880d71caa731fb7cb284349670f99c0cb074d
Karthikeyan A <karthikaruna@live.com>
2019-03-18 23:34:16 +0530

    Misc

--- addon/components/keypad-component/component.js
+++ addon/components/keypad-component/component.js
@@ -1,6 +1,22 @@
 import Component from '@ember/component';
 import layout from './template';
+import { computed } from '@ember/object';
 
 export default Component.extend({
-  layout
+  layout,
+  keys: computed({
+    get() {
+      return {
+        1: '',
+        2: 'ABC',
+        3: 'DEF',
+        4: 'GHI',
+        5: 'JKL',
+        6: 'MNO',
+        7: 'PQRS',
+        8: 'TUV',
+        9: 'WXYZ'
+      };
+    }
+  })
 });
